e3a0788d207a4cb81b9dd94ba34ad93114aeb735
Ami Sheth <amisheth26@g.ucla.edu>
2020-01-23 19:49:55 +0000

    completed Q4.1 by modifying runSim script to contain desired arguments and report avg MSE for both methods; still need to test if edited script works

--- hw1/runSim.R
+++ hw1/runSim.R
@@ -1,28 +1,52 @@
-## parsing command arguments
-for (arg in commandArgs(TRUE)) {
-  eval(parse(text=arg))
-}
+set.seed(seed)
+mean_prime_est = 0
+mean_samp_est = 0
 
-## check if a given integer is prime
-isPrime = function(n) {
-  if (n <= 3) {
+for (i in 1:rep){
+  ## parsing command arguments
+  for (arg in commandArgs(TRUE)) {
+    eval(parse(text=arg))
+  }
+  
+  ## check if a given integer is prime
+  isPrime = function(n) {
+    if (n <= 3) {
+      return (TRUE)
+    }
+    if (any((n %% 2:floor(sqrt(n))) == 0)) {
+      return (FALSE)
+    }
     return (TRUE)
   }
-  if (any((n %% 2:floor(sqrt(n))) == 0)) {
-    return (FALSE)
+  
+  ## estimate mean only using observation with prime indices
+  estMeanPrimes = function (x) {
+    n = length(x)
+    ind = sapply(1:n, isPrime)
+    return (mean(x[ind]))
   }
-  return (TRUE)
+  
+  ## simulate data
+  if (dist=="gaussian"){
+    x=rnorm(n)
+  } else if (dist=="t1"){
+    x=rt(n,1)
+  } else if (dist=="t5"){
+    x=rt(n,5)
+  } else {
+    stop("Distribution Not Recognized")
+  }
+  
+  ## mean estimate for both methods 
+  mean_prime_est[i] = estMeanPrimes(x)
+  mean_samp_est[i] = mean(x)
+  
 }
 
-## estimate mean only using observation with prime indices
-estMeanPrimes = function (x) {
-  n = length(x)
-  ind = sapply(1:n, isPrime)
-  return (mean(x[ind]))
-}
+## MSE formula give that true mean = 0
+mse_prime_avg = sum(mean_prime_est^2)/rep
+mse_samp_avg = sum(mean_samp_est^2)/rep
 
-# simulate data
-x = rnorm(n)
-
-# estimate mean
-estMeanPrimes(x)
+## print values 
+mse_prime_avg
+mse_samp_avg
